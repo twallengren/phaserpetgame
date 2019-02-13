c0096bb6e656e4a74458639d716d82f5d0b33fa9
Toren Wallengren <tw@Torens-iMac-Pro.local>
2019-02-12 18:17:16 -0700

    pet rotates and changes stats

--- js/main.js
+++ js/main.js
@@ -1,187 +1,181 @@
 // create a new scene
-let gameScene = new Phaser.Scene('Game');
+let gameScene = new Phaser.Scene("Game");
 
 // some parameters for our scene
-gameScene.init = function () {
-
+gameScene.init = function() {
   // game stats
   this.stats = {
     health: 100,
-    fun: 100,
-  }
-
+    fun: 100
+  };
 };
 
 // load asset files for our game
-gameScene.preload = function () {
-
+gameScene.preload = function() {
   // load assets
-  this.load.image('backyard', 'assets/images/backyard.png');
-  this.load.image('apple', 'assets/images/apple.png');
-  this.load.image('candy', 'assets/images/candy.png');
-  this.load.image('rotate', 'assets/images/rotate.png');
-  this.load.image('toy', 'assets/images/rubber_duck.png');
+  this.load.image("backyard", "assets/images/backyard.png");
+  this.load.image("apple", "assets/images/apple.png");
+  this.load.image("candy", "assets/images/candy.png");
+  this.load.image("rotate", "assets/images/rotate.png");
+  this.load.image("toy", "assets/images/rubber_duck.png");
 
   // load spritesheet
-  this.load.spritesheet('pet', 'assets/images/pet.png', {
-
+  this.load.spritesheet("pet", "assets/images/pet.png", {
     // sprite properties
     frameWidth: 97,
     frameHeight: 83,
     margin: 1,
-    spacing: 1,
-
-  })
-
+    spacing: 1
+  });
 };
 
 // executed once, after assets were loaded
-gameScene.create = function () {
-
+gameScene.create = function() {
   // add game background
-  this.bg = this.add.sprite(0, 0, 'backyard').setOrigin(0, 0).setInteractive()
+  this.bg = this.add
+    .sprite(0, 0, "backyard")
+    .setOrigin(0, 0)
+    .setInteractive();
 
   // add event listener for background
-  this.bg.on('pointerdown', this.placeItem, this)
+  this.bg.on("pointerdown", this.placeItem, this);
 
   // add pet
-  this.pet = this.add.sprite(100, 200, 'pet', 0).setInteractive()
+  this.pet = this.add.sprite(100, 200, "pet", 0).setInteractive();
 
   // make pet draggable
-  this.input.setDraggable(this.pet)
+  this.input.setDraggable(this.pet);
 
   // follow pointer (mouse/finger) when dragging
-  this.input.on('drag', function (pointer, gameObject, dragX, dragY) {
-
+  this.input.on("drag", function(pointer, gameObject, dragX, dragY) {
     // make sprite be located at the drag coordinates
-    gameObject.x = dragX
-    gameObject.y = dragY
-
-  })
+    gameObject.x = dragX;
+    gameObject.y = dragY;
+  });
 
   // create ui
-  this.createUI()
-
+  this.createUI();
 };
 
 // create ui
-gameScene.createUI = function () {
-
+gameScene.createUI = function() {
   // add apple button and set/define interactivity
-  this.appleButton = this.add.sprite(72, 570, 'apple').setInteractive()
-  this.appleButton.customStats = { health: 20, fun: 0 }
-  this.appleButton.on('pointerdown', this.pickItem)
+  this.appleButton = this.add.sprite(72, 570, "apple").setInteractive();
+  this.appleButton.customStats = { health: 20, fun: 0 };
+  this.appleButton.on("pointerdown", this.pickItem);
 
   // add candy button and set/define interactivity
-  this.candyButton = this.add.sprite(144, 570, 'candy').setInteractive()
-  this.candyButton.customStats = { health: -10, fun: 10 }
-  this.candyButton.on('pointerdown', this.pickItem)
+  this.candyButton = this.add.sprite(144, 570, "candy").setInteractive();
+  this.candyButton.customStats = { health: -10, fun: 10 };
+  this.candyButton.on("pointerdown", this.pickItem);
 
   // add toy button and set/define interactivity
-  this.toyButton = this.add.sprite(216, 570, 'toy').setInteractive()
-  this.toyButton.customStats = { health: 0, fun: 15 }
-  this.toyButton.on('pointerdown', this.pickItem)
+  this.toyButton = this.add.sprite(216, 570, "toy").setInteractive();
+  this.toyButton.customStats = { health: 0, fun: 15 };
+  this.toyButton.on("pointerdown", this.pickItem);
 
   // add rotate button and set/define interactivity
-  this.rotateButton = this.add.sprite(288, 570, 'rotate').setInteractive()
-  this.rotateButton.on('pointerdown', this.rotatePet)
+  this.rotateButton = this.add.sprite(288, 570, "rotate").setInteractive();
+  this.rotateButton.customStats = { fun: 20 };
+  this.rotateButton.on("pointerdown", this.rotatePet);
 
   // array with all buttons
-  this.buttons = [this.appleButton, this.candyButton, this.toyButton, this.rotateButton]
+  this.buttons = [
+    this.appleButton,
+    this.candyButton,
+    this.toyButton,
+    this.rotateButton
+  ];
 
   // ui is not blocked
-  this.uiBlocked = false
+  this.uiBlocked = false;
 
   // refresh ui
-  this.uiReady()
-
-}
+  this.uiReady();
+};
 
 // rotate pet
-gameScene.rotatePet = function () {
-
+gameScene.rotatePet = function() {
   // check if the user interface is blocked
   // prevents sprite from rotating during actions
-  if (this.scene.uiBlocked) return
+  if (this.scene.uiBlocked) return;
 
   // make sure ui is ready
-  this.scene.uiReady()
+  this.scene.uiReady();
 
   // block the ui for rotation action
-  this.scene.uiBlocked = true
+  this.scene.uiBlocked = true;
 
   // dim the rotate icon
-  this.alpha = 0.5
+  this.alpha = 0.5;
 
-  let scene = this.scene
+  // rotation tween
+  let rotateTween = this.scene.tweens.add({
+    targets: this.scene.pet,
+    duration: 600,
+    angle: 360,
+    pause: false,
+    callbackScope: this,
+    onComplete: function(tween, sprites) {
+      // increase fun
+      this.scene.stats.fun += this.customStats.fun;
 
-  setTimeout(function () {
+      // set UI to ready
+      this.scene.uiReady();
 
-    // set scene back to ready
-    scene.uiReady()
-
-  }, 2000)
-
-}
+      console.log(this.scene.stats);
+    }
+  });
+};
 
 // pick item
-gameScene.pickItem = function () {
-
+gameScene.pickItem = function() {
   // check if the user interface is blocked
   // prevents items from being picked during actions
-  if (this.scene.uiBlocked) return
+  if (this.scene.uiBlocked) return;
 
   // make sure the ui is ready
-  this.scene.uiReady()
+  this.scene.uiReady();
 
   // select item
-  this.scene.selectedItem = this
+  this.scene.selectedItem = this;
 
   // change transparency of selected sprite
-  this.alpha = 0.5
-
-}
+  this.alpha = 0.5;
+};
 
 // set ui to ready
-gameScene.uiReady = function () {
-
+gameScene.uiReady = function() {
   // make sure nothing is being selected
-  this.selectedItem = null
+  this.selectedItem = null;
 
   // no transparency (alpha) on all buttons
   for (button in this.buttons) {
-
-    this.buttons[button].alpha = 1
-
+    this.buttons[button].alpha = 1;
   }
 
   // scene must be unblocked
-  this.uiBlocked = false
-
-}
+  this.uiBlocked = false;
+};
 
 // place a new item in gamescene
-gameScene.placeItem = function (pointer, localX, localY) {
-
+gameScene.placeItem = function(pointer, localX, localY) {
   // check that an item was selected
-  if (!this.selectedItem) return
+  if (!this.selectedItem) return;
 
   // create a new item at the click position
-  let newItem = this.add.sprite(localX, localY, this.selectedItem.texture.key)
+  let newItem = this.add.sprite(localX, localY, this.selectedItem.texture.key);
 
   // pet stats
   for (stat in this.selectedItem.customStats) {
     if (this.selectedItem.customStats.hasOwnProperty(stat)) {
-
-      this.stats[stat] += this.selectedItem.customStats[stat]
-
+      this.stats[stat] += this.selectedItem.customStats[stat];
     }
   }
 
   // clear UI
-  this.uiReady()
-
-}
+  this.uiReady();
+};
 
 // our game's configuration
 let config = {
@@ -189,9 +183,9 @@ let config = {
   width: 360,
   height: 640,
   scene: gameScene,
-  title: 'Virtual Pet',
+  title: "Virtual Pet",
   pixelArt: false,
-  backgroundColor: 'ffffff'
+  backgroundColor: "ffffff"
 };
 
 // create the game, and pass it the configuration
